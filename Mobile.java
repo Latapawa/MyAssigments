3c43e6f4ee6434dd8fe7c716009922ed379468d9
Latapawa <124447527+Latapawa@users.noreply.github.com>
2023-02-04 21:14:48 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		
+		String mobileBrandNmae="Sumsung";
+		System.out.println(mobileBrandNmae);
+		
+		char mobileLogo='S';
+		System.out.println(mobileLogo);
+		
+		short noOfMobilePiece=2;
+		System.out.println(noOfMobilePiece);
+		
+		int modelNumber=456123;
+		System.out.println(modelNumber);
+		
+		long mobileImeiNumber =7895655565655656l;
+		System.out.println(mobileImeiNumber);
+		
+		float mobilePrice=18006.3f;
+		System.out.println(mobilePrice);
+		
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+				
+	}
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int noOfWheels =2;
+System.out.println(noOfWheels);
+
+short noOfMirrors=2;
+System.out.println(noOfMirrors);
+
+long chassisNumber=2;
+System.out.println(chassisNumber);
+
+boolean isPunctured=true;
+System.out.println(isPunctured);
+
+String bikeName="Royal infied";//String is class name 
+System.out.println(bikeName);
+
+double runningKM=1744.20;
+System.out.println(runningKM);
+
+
+	}
+
+}
